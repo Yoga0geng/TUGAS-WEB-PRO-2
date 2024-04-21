6c31ddf848253327a046d30bc693ab8c93f609a3
YogaAgustirahayuM.F <ogenkepleu234@gmail.com>
2024-04-21 21:21:41 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Yoga</li>
+        <li>Nama Belakang : Agustirahayu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Mutira Taman Palem, Blok B1</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Ciamis</li>
+    </ul>
+    <h4>Mata Kuliah Favorit</h4>
+    <ul type="square">
+        <li>Web Programming 2</li>
+        <li></li>
+    </ul>
+</section>
